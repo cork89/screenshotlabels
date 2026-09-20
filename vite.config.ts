b051b0055da58521fae4e96706cef39c41f36d93
cork89 <smlarabell@gmail.com>
2026-09-20 09:20:59 -0400

    license + github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  base: "./",
   fmt: {},
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
